59c21456f404248ce98794776fcfdba49d3e8a7d
wontem <2wontem@gmail.com>
2016-09-23 16:02:46 +0300

    render all tiles

--- src/containers/layers/maze_layer.jsx
+++ src/containers/layers/maze_layer.jsx
@@ -1,15 +1,17 @@
 import {Layer} from 'shared/components/canvas';
-import {canvasTransform} from 'helpers/canvas_helpers';
+import {canvasTransform, drawPath} from 'helpers/canvas_helpers';
 
 import {connect} from 'react-redux';
 import {createSelector} from 'reselect';
+import {tiles} from 'selectors/grids_selectors';
 import {borderPoints} from 'selectors/maze_selectors';
 import {tilesTransform, size} from 'selectors/canvas_selectors';
 
 @connect(createSelector(
-    [borderPoints, tilesTransform, size],
-    (borderPoints, tilesTransform, size) => ({
+    [tiles, borderPoints, tilesTransform, size],
+    (tiles, borderPoints, tilesTransform, size) => ({
         ...size,
+        tiles,
         borders: borderPoints,
         transform: tilesTransform,
     })
@@ -17,13 +19,18 @@ import {tilesTransform, size} from 'selectors/canvas_selectors';
 export default class MazeLayer extends Layer {
     clearAndDraw() {
         const ctx = this.ctx;
-        const {borders} = this.props;
+        const {borders, tiles} = this.props;
 
         canvasTransform(ctx, this.props.transform);
         ctx.lineWidth = 1 / 10;
         ctx.lineJoin = 'round';
         ctx.strokeStyle = 'palegreen';
-        ctx.fillStyle = 'palegreen';
+        ctx.fillStyle = '#111';
+
+        tiles.forEach(({points}) => {
+            drawPath(ctx, points, true);
+            ctx.fill();
+        });
 
         ctx.beginPath();
         borders.forEach(([startPoint, endPoint]) => {
